3099f28b8c7866c5ccbe503764ba56411201e193
softkern <hi@softkern.com>
2024-12-07 12:43:01 +0800

    fix: some details problem

--- uno.config.ts
+++ uno.config.ts
@@ -4,9 +4,10 @@ export default defineConfig({
   shortcuts: {
     btn: 'px-4 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-300',
     heading: 'font-bold tracking-tight',
-    card: 'p-6 rounded-xl bg-white dark:bg-gray-800 shadow-sm hover:shadow-md transition-all duration-300',
-    'bg-base': 'bg-gray-50 dark:bg-gray-900',
+    card: 'p-6 rounded-xl bg-gray-50 dark:bg-gray-800 shadow-sm hover:shadow-md transition-all duration-300',
+    'bg-base': 'bg-white dark:bg-gray-900',
     'color-base': 'text-gray-800 dark:text-gray-200',
+    'color-mark': 'text-primary-700 dark:text-primary-500',
   },
   theme: {
     colors: {
